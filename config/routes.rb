4904e4a9f93931a4894b6faf90ea206fb6756031
Administrator <jonatan.kapps.dev@gmail.com>
2017-06-03 10:38:27 -0300

    First github commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
+  #O resources cria todas as rotas necessárias para o CRUD de uma
+  #entidade no rails
+  #resources :products
 
   get 'home/index'
   root 'home#index'
@@ -8,5 +10,6 @@ Rails.application.routes.draw do
     get 'products/new'
     post 'products/create'
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
